5039fa1640b255092f15829a10c940b9bf098464
IZhuhan <zhugan.irina@gmail.com>
2026-01-13 18:33:31 +0200

    added home-work-41

--- home-work-38/src/components/Input/Input.jsx
+++
@@ -1,9 +0,0 @@
-const Input = (props) => {
-  const handleChange = () => console.log('Input');
-
-  return (
-    <input onChange={handleChange} type={props.type} placeholder={props.placeholder} />
-  )
-}
-
-export default Input;
