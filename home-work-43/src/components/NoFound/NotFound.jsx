6db58edb7ad0099b05858f5e441a85a207c4e8a1
IZhuhan <zhugan.irina@gmail.com>
2026-01-20 16:07:47 +0200

    added markup and routing for home-work-43

---
+++ home-work-43/src/components/NoFound/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from "react-router";
+
+const NotFound = () => {
+  return (
+    <div>
+      <h1>Page not found</h1>
+      <Link to={'/'}>Go to home page</Link>
+    </div>
+  )
+}
+export default NotFound
