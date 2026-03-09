ce777cf39637f846311fb4ccd9e85534fa100d83
IZhuhan <zhugan.irina@gmail.com>
2026-03-09 15:25:39 +0200

    added home-work-45

--- home-work-45/src/App.jsx
+++ home-work-45/src/App.jsx
@@ -1,8 +1,20 @@
 import './App.css'
+import {useDispatch} from "react-redux";
+import {increment} from "./store/slices/counterSlice.js";
 
 function App() {
+  const dispatch = useDispatch();
+  const handleIncrement = () => {
+    dispatch(increment());
+  }
+
   return (
-    <div>App</div>
+    <>
+      <h1>Counter: {0}</h1>
+      <button onClick={handleIncrement}>Increment</button>
+      <button>Decrement</button>
+      <button>Reset</button>
+    </>
   )
 }
 
